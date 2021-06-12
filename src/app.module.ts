aa91854e5ad44ee654df765f466e1563f33f66e6
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-06-12 23:05:58 +0200

    generate products CRUD module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_CONNECTION),
+    MongooseModule.forRoot(process.env.DB_CONNECTION, { autoCreate: true }),
+    ProductsModule,
   ],
   controllers: [],
   providers: [],
