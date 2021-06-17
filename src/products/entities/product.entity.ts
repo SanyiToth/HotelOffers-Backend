227c7d0997b6520dd012fa28fcc5941ca1591958
Toth <sandor.toth@hotmail.com>
2021-06-17 20:33:58 +0200

    product.entity has been changed

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,24 +1,62 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Types, Schema as MongooseSchema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types, Schema as MongooseSchema } from "mongoose";
+
+
+
+@Schema()
+export class RatingInfo extends Document {
+  @Prop({ required: true })
+  rating: number;
+
+  @Prop({ required: true })
+  numberOfRating: number;
+}
+
+export const RatingSchema = SchemaFactory.createForClass(RatingInfo);
 
 export type ProductDocument = Product & Document;
 
 @Schema()
 export class Product {
+
+  @Prop()
+  status: string;
+
   @Prop({ required: true })
-  name: string;
+  heading: string;
+
+  @Prop({ required: true })
+  details: string;
+
+  @Prop()
+  startDate: Date;
+
+  @Prop()
+  endDate: Date;
+  
+  @Prop()
+  availableOffers?: number;
 
   @Prop()
   price: number;
 
   @Prop()
-  providerId: number;
+  images: string;
 
   @Prop()
-  type: string;
+  providerId: number;
 
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Provider' })
   provider: Types.ObjectId;
+
+  @Prop()
+  description: string;
+
+  @Prop({ type: [String], default: [] })
+  tags: string;
+
+  @Prop({ type: RatingSchema })
+  ratingInfo: RatingInfo;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
