f6f85229a41e7532896dd950199638a82fb3df6e
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-06-17 18:13:27 +0200

    add address schema to provider

--- src/providers/entities/provider.entity.ts
+++ src/providers/entities/provider.entity.ts
@@ -1,6 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+
+@Schema()
+export class Address extends Document {
+  @Prop({ required: true })
+  city: string;
+
+  @Prop()
+  streetName: string;
+
+  @Prop()
+  streetNumber: string;
+
+  @Prop()
+  postalCode: number;
+
+  @Prop({ required: true })
+  country: string;
+}
+export const AddressSchema = SchemaFactory.createForClass(Address);
+
 export type ProviderDocument = Provider & Document;
 
 @Schema()
@@ -8,8 +28,8 @@ export class Provider {
   @Prop({ required: true })
   name: string;
 
-  @Prop()
-  address: string;
+  @Prop({ type: AddressSchema })
+  address: Address;
 }
 
 export const ProviderSchema = SchemaFactory.createForClass(Provider);
