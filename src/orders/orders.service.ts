248c7685f94510c41014669e47ec258ba926084f
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-06-21 21:47:16 +0200

    add CREATE, READ methods

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,19 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UseGuards } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Order, OrderDocument } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
+  constructor(
+    @InjectModel(Order.name)
+    private readonly model: Model<OrderDocument>,
+  ) {}
+
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const created = new this.model(createOrderDto);
+    return created.save();
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(): Promise<Order[]> {
+    return await this.model.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  findOne(id: string): Promise<Order> {
+    return this.findOrder(id);
   }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
@@ -22,5 +31,30 @@ export class OrdersService {
 
   remove(id: number) {
     return `This action removes a #${id} order`;
+  }
+
+  private async findOrder(id: string, populated = false) {
+    let order;
+
+    try {
+      if (populated) {
+        order = await this.model
+          .findById(id)
+          .populate('provider')
+          .populate('product')
+          .exec();
+      } else {
+        order = await this.model.findById(id).exec();
+      }
+    } catch (error) {
+      console.log('error', error);
+      throw new NotFoundException('Could not find order!');
+    }
+
+    if (!order) {
+      throw new NotFoundException('Order does not exist!');
+    }
+
+    return order;
   }
 }
