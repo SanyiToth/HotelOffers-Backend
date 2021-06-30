d63865341474a038f4f14c8b21dd8e5372eeccc9
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-06-30 19:58:21 +0200

    rename API endpoint (/products to /offers)

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,7 +13,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 
-@Controller('products')
+@Controller('offers')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
